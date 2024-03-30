9cd64bee515b287d2ff8a7ec7059b70e436e2275
bl33h <per21371@uvg.edu.gt>
2024-03-30 12:35:57 -0600

    proto works in both, general idea of option 1

--- src/client.cpp
+++ src/client.cpp
@@ -6,10 +6,88 @@ FileName: client.cpp
 Creation: 19/03/2024
 Last modification: 30/03/2024
 ------------------------------------------------------------------------------*/
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include "chat.pb.h"
 #include <iostream>
+#include <unistd.h>
+
+void menu();
+void help();
+void sendMessage(int sock);
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    // initialize protocol buffers library
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    // socket creation
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "\nSocket creation error\n";
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080); // port number
+
+    // binary ip convertion
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        std::cerr << "\nInvalid address/ Address not supported\n";
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "\nConnection Failed\n";
+        return -1;
+    }
+
+    menu();
+
+    int command = 0;
+    do {
+        std::cout << "Enter command: ";
+        std::cin >> command;
+
+        switch (command) {
+            case 1:
+                sendMessage(sock);
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                help();
+                break;
+
+            case 7:
+                std::cout << "Exiting...\n";
+                break;
+
+            default:
+                std::cout << "Invalid command. Please try again.\n";
+                break;
+        }
+    } while (command != 7);
+
+    // socket cleanup
+    close(sock);
+    google::protobuf::ShutdownProtobufLibrary();
+
+    return 0;
+}
 
 // available options menu
 void menu() {
@@ -46,47 +124,27 @@ void help() {
     }
 }
 
-int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
+void sendMessage(int sock) {
+    chat::MessageCommunication msg;
 
-    // socket creation
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "\n Socket creation error \n";
-        return -1;
-    }
+    // sender
+    msg.set_sender("ClientUsername"); 
 
-    // display options
-    menu();
+    // recipient
+    msg.set_recipient("General"); 
+    std::string messageContent;
 
-    // main loop
-    int command = 0;
-    do {
-        std::cout << "Enter command: ";
-        std::cin >> command;
+    // input
+    std::cout << "Enter your message: ";
+    std::cin.ignore();
+    std::getline(std::cin, messageContent);
+    msg.set_message(messageContent);
 
-        switch (command) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                help();
-                break;
-            case 7:
-                std::cout << "Exiting...\n";
-                break;
-            default:
-                std::cout << "Invalid command. Please try again.\n";
-                break;
-        }
-    } while (command != 7);
+    // serialize message
+    std::string serializedMsg;
+    msg.SerializeToString(&serializedMsg);
 
-    return 0;
+    send(sock, serializedMsg.c_str(), serializedMsg.size(), 0);
+
+    std::cout << "Message sent to server.\n";
 }
--- src/server.cpp
+++ src/server.cpp
@@ -6,69 +6,39 @@ FileName: server.cpp
 Creation: 19/03/2024
 Last modification: 19/03/2024
 ------------------------------------------------------------------------------*/
+#include <iostream>
+#include <string>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <iostream>
-#include <cstring>
-#include <thread>
-#include <vector>
-
-void handleClient(int clientSocket) {
-    char buffer[1024];
-    memset(buffer, 0, 1024);
-
-    ssize_t bytesRead = read(clientSocket, buffer, 1024);
-    if (bytesRead > 0) {
-        std::cout << "-> message received from client: " << buffer << std::endl;
-
-        std::string response = "--- message received successfully ---";
-        write(clientSocket, response.c_str(), response.length());
-    } else {
-        std::cout << "!error reading client message or connection closed" << std::endl;
-    }
-
-    close(clientSocket);
-}
+#include "chat.pb.h"  // Include the protobuf generated header file
 
 int main() {
     int serverFd, newSocket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
+    char buffer[1024] = {0};
 
-    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
+    serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    if (bind(serverFd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(serverFd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    bind(serverFd, (struct sockaddr *)&address, sizeof(address));
+    listen(serverFd, 3);
 
-    while (true) {
-        if ((newSocket = accept(serverFd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
+    newSocket = accept(serverFd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
 
-        std::thread t(handleClient, newSocket);
-        t.detach();
-    }
+    // Read message from client
+    read(newSocket, buffer, 1024);
+    chat::MessageCommunication msg;
+    msg.ParseFromArray(buffer, 1024);
 
+    std::cout << "Message received: " << msg.message() << std::endl;
+
+    close(newSocket);
+    close(serverFd);
     return 0;
 }
