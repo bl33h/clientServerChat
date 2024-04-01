d9534e5c71e909bf47883517d898ce6e543be391
Mendezg1 <eurocoparich@gmail.com>
2024-03-31 23:08:26 -0600

    Broadcast gets Message

--- src/client.c
+++ src/client.c
@@ -15,34 +15,6 @@ Last modification: 31/03/2024
 #include <stdlib.h>
 #include <stdio.h>
 
-<<<<<<< HEAD
-#define MAX_BUF_SIZE 1024
-
-char username[MAX_BUF_SIZE];
-int clientSocket;
-
-void print_menu() {
-    printf("\n--- Available options ---\n");
-    printf("1. Chat with everyone (general room)\n");
-    printf("2. View connected users\n");
-    printf("3. Change status\n");
-    printf("4. User info\n");
-    printf("5. Exit\n");
-}
-
-void send_message() {
-    char message_content[MAX_BUF_SIZE];
-    printf("Enter your message: ");
-    fgets(message_content, MAX_BUF_SIZE, stdin);
-    message_content[strcspn(message_content, "\n")] = 0; // Remove newline
-
-    Chat__MessageCommunication msg = CHAT__MESSAGE_COMMUNICATION__INIT;
-    char recipient[] = "everyone";
-    msg.sender = username;
-    msg.recipient = recipient;
-    msg.message = message_content;
-
-=======
 // --- variables ---
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
@@ -92,56 +64,27 @@ void send_message(int sock) {
     msg.message = message_content;
 
     // serialize the message
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
     unsigned len = chat__message_communication__get_packed_size(&msg);
     uint8_t *buf = malloc(len);
     chat__message_communication__pack(&msg, buf);
 
-<<<<<<< HEAD
-    send(clientSocket, buf, len, 0);
-=======
     // send the message
     send(sock, buf, len, 0);
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
 
     free(buf);
 }
 
-<<<<<<< HEAD
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        printf("Usage: %s <username> <serverIP> <IPport>\n", argv[0]);
-        return 1;
-    }
-
-    strcpy(username, argv[1]);
-    char* serverIP = argv[2];
-    int IPport = atoi(argv[3]);
-
-    struct sockaddr_in serv_addr;
-    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-=======
 int main() {
     int sock;
     struct sockaddr_in serv_addr;
 
     // create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
         perror("Socket creation error");
         return -1;
     }
 
     serv_addr.sin_family = AF_INET;
-<<<<<<< HEAD
-    serv_addr.sin_port = htons(IPport);
-    if(inet_pton(AF_INET, serverIP, &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/Address not supported \n");
-        return -1;
-    }
-
-    if (connect(clientSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-=======
     serv_addr.sin_port = htons(SERVER_PORT);
 
     // ip convertion
@@ -152,35 +95,10 @@ int main() {
 
     // server connection
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
         perror("Connection Failed");
         return -1;
     }
 
-<<<<<<< HEAD
-    printf("Connected to the server as %s.\n", username);
-
-    int option;
-    do {
-        print_menu();
-        printf("Select an option: ");
-        scanf("%d", &option);
-        getchar(); // consume newline
-
-        switch (option) {
-            case 1: // Chat with everyone
-                send_message();
-                break;
-            case 5: // Exit
-                printf("Exiting...\n");
-                close(clientSocket);
-                exit(0);
-            default:
-                printf("Option not implemented.\n");
-        }
-    } while (option != 5);
-
-=======
     printf("Connected to the server.\n");
 
     // main loop
@@ -208,6 +126,5 @@ int main() {
     }
 
     close(sock);
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -10,77 +10,14 @@ Last modification: 31/03/2024
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-<<<<<<< HEAD
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-=======
-#include <sys/socket.h>
-#include <netinet/in.h>
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
 #include <pthread.h>
 #include "chat.pb-c.h"
 
 #define PORT 8080
 #define MAX_CLIENTS 10
 
-<<<<<<< HEAD
-int client_sockets[MAX_CLIENTS];
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void broadcast_message(uint8_t *buffer, ssize_t buf_len, int sender_sock) {
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] != 0 && client_sockets[i] != sender_sock) {
-            if (send(client_sockets[i], buffer, buf_len, 0) == -1) {
-                perror("send");
-            }
-        }
-    }
-    pthread_mutex_unlock(&clients_mutex);
-}
-
-void *handle_client(void *arg) {
-    int client_sock = *(int *)arg;
-    free(arg);
-    uint8_t buffer[1024];
-    ssize_t bytes_received;
-
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-    getpeername(client_sock, (struct sockaddr *)&addr, &addr_len);
-
-    while (1) {
-        bytes_received = recv(client_sock, buffer, 1024, 0);
-        if (bytes_received <= 0) {
-            pthread_mutex_lock(&clients_mutex);
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] == client_sock) {
-                    client_sockets[i] = 0;
-                    break;
-                }
-            }
-            pthread_mutex_unlock(&clients_mutex);
-            close(client_sock);
-            break;
-        }
-
-        Chat__MessageCommunication *msg;
-        msg = chat__message_communication__unpack(NULL, bytes_received, buffer);
-        if (msg != NULL) {
-            char ip_str[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &addr.sin_addr, ip_str, INET_ADDRSTRLEN);
-            // Modified to print in the desired format
-            printf("message from %s %s: %s\n", msg->sender, ip_str, msg->message);
-            broadcast_message(buffer, bytes_received, client_sock);
-            chat__message_communication__free_unpacked(msg, NULL);
-        } else {
-            fprintf(stderr, "Error unpacking incoming message\n");
-        }
-    }
-
-=======
 // Thread function to handle clients
 void* handle_client(void* arg) {
     int clientSocket = *(int*)arg;
@@ -98,49 +35,16 @@ void* handle_client(void* arg) {
         }
         printf("User %s connected\n", msg->sender);
         // Free the unpacked message
-        chat__message_communication__free_unpacked(msg, NULL);
-    }
-
-    // Communication with client
-    while ((bytes_read = read(clientSocket, buffer, 1024)) > 0) {
-        msg = chat__message_communication__unpack(NULL, bytes_read, buffer);
-        if (msg == NULL) {
-            fprintf(stderr, "Error unpacking incoming message\n");
-            continue;
-        }
-        printf("Message from %s: %s\n", msg->sender, msg->message);
-
-        // Broadcasting message to other clients should be implemented here
-
+        printf("Message from client: %s\n", msg->message);
         chat__message_communication__free_unpacked(msg, NULL);
     }
 
     // Cleanup
     close(clientSocket);
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
     return NULL;
 }
 
 int main() {
-<<<<<<< HEAD
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in server_addr = {0};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_sock, MAX_CLIENTS) == -1) {
-=======
     int server_fd, new_socket, *new_sock;
     struct sockaddr_in address;
     int opt = 1;
@@ -166,50 +70,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
     if (listen(server_fd, MAX_CLIENTS) < 0) {
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-<<<<<<< HEAD
-    printf("Server started on port %d\n", PORT);
-
-    while (1) {
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int new_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (new_sock == -1) {
-=======
     // Accepting clients
     while(1) {
         if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen))<0) {
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
             perror("accept");
             continue;
         }
 
-<<<<<<< HEAD
-        pthread_mutex_lock(&clients_mutex);
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (client_sockets[i] == 0) {
-                client_sockets[i] = new_sock;
-                break;
-            }
-        }
-        pthread_mutex_unlock(&clients_mutex);
-
-        int *new_sock_ptr = malloc(sizeof(int));
-        *new_sock_ptr = new_sock;
-        pthread_t thread_id;
-        if (pthread_create(&thread_id, NULL, handle_client, new_sock_ptr) != 0) {
-            perror("pthread_create");
-=======
         pthread_t thread_id;
         new_sock = malloc(sizeof(int));
         *new_sock = new_socket;
         if (pthread_create(&thread_id, NULL, handle_client, (void*) new_sock) != 0) {
             perror("pthread_create failed");
->>>>>>> 839afa22ae8ae452d329f2a96e2cd40fd2cb82d3
         }
     }
 
